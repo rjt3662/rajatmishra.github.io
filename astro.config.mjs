597e225b351a2122e4e3141dbcd3a63d3d827171
Rajat Mishra <rajat.m324@gmail.com>
2026-03-09 12:03:04 +0530

    Fix Tailwind integration by adding @astrojs/tailwind and removing @tailwind directives from CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: "https://rjt3662.github.io",
   base: "/rajatmishra.github.io/",
-  outDir: "./docs"
+  outDir: "./docs",
+  integrations: [tailwind()]
 });
