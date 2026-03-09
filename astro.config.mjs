d0a5849e8612b92b99fdde4fdbb3e90b48e95dd8
Rajat Mishra <rajat.m324@gmail.com>
2026-03-09 12:21:05 +0530

    Update base path to root now that repo is a user site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: "https://rjt3662.github.io",
-  base: "/rajatmishra.github.io/",
+  // repository moved to a user site; assets are now served from root
+  base: "/",
   outDir: "./docs",
   integrations: [tailwind()]
 });
