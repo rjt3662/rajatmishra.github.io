056fbeaaca52ead546891922ac61196fa3da3081
Rajat Mishra <rajat.m324@gmail.com>
2026-03-09 11:50:34 +0530

    Fix base path and CSS loading for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: "https://rjt3662.github.io",
-  base: "/rajatmishra.github.io/",
+  base: "/",
   outDir: "./docs"
 });
